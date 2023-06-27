c7b0ad7dbd22014954dcf35a30f0ddea05be3062
pychangeric <pychangeric@gmail.com>
2023-06-27 14:31:56 +0300

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :set_page_views, only: [:show]
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -7,14 +7,19 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if @page_views < 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: 'Maximum page views reached. Please subscribe to continue.' }, status: :unauthorized
+    end
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def set_page_views
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    @page_views = session[:page_views]
   end
-
 end
